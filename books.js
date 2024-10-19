02e57e2e6c4066eab3f765a5d4a03f32fe07be15
MahmoudHamo1 <mahmoudhamo15@gmail.com>
2024-10-19 20:27:59 +0300

    test 1

---
+++ books.js
@@ -0,0 +1,15 @@
+const Books = {
+  Trending: [
+    { bookName: "", author: "", price: 0.0, rate: "" },
+    { bookName: "", author: "", price: 0.0, rate: "" },
+  ],
+
+  popular: [
+    { bookName: "", author: "", price: 0.0, rate: "" },
+    { bookName: "", author: "", price: 0.0, rate: "" },
+  ],
+  all: [
+    { bookName: "", author: "", price: 0.0, rate: "" },
+    { bookName: "", author: "", price: 0.0, rate: "" },
+  ],
+};
